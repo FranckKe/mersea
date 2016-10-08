570d64e93d0c6c4035ba46b8f46391dc0f5dae3f
Franck Kerbiriou <franck.kerbiriou@gmail.com>
2016-10-08 19:23:19 +0200

    Prevent map to load on other pages

--- app/assets/javascripts/pages_index.js
+++ app/assets/javascripts/pages_index.js
@@ -4,15 +4,18 @@
 $(document).on('turbolinks:load', function() {
 
   // Map init
-  var map = L.map('map').setView([48.2520, -3.9301], 9);
+  var mapElement = $("#map");
 
-  var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+  if (mapElement.length > 0) {
+    var map = L.map('map').setView([48.2520, -3.9301], 9);
+    var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 
-  L.tileLayer(
-    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; ' + mapLink,
-      maxZoom: 18
-    }).addTo(map);
+    L.tileLayer(
+      'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; ' + mapLink,
+        maxZoom: 18
+      }).addTo(map);
 
-  map.zoomControl.setPosition('topright');
+    map.zoomControl.setPosition('topright');
+  }
 });
