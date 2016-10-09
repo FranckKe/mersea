54b5598e6e9abbed481ff03c2928458f9d7c32c1
Franck Kerbiriou <franck.kerbiriou@gmail.com>
2016-10-09 17:36:16 +0200

    Implement form

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -13,17 +13,18 @@ class ReportsController < ApplicationController
     @report = Report.new(report_params)
 
     if @report.save
+      flash.now[:success] = 'Merci pour votre signalement'
       redirect_to root_path
     else
       flash.now[:alert] = @report.errors.messages.first
-      render action: :new
+      redirect_to Tracer.find(params[:report][:tracer_id])
     end
   end
 
   private
 
   def report_params
-    params.permit(:tracer_id, :name, :email, :quantity, :address, :longitude,
-      :latitude, :description, :photo, :reported_at)
+    params.require(:report).permit(:tracer_id, :name, :email, :quantity, :address, :longitude,
+                                   :latitude, :description, :photo, :reported_at)
   end
 end
