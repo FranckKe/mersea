176a7a655a9cb622cb03c0075de9815b835556d6
Pierre Charpentier <charpentier.pierre@outlook.com>
2016-10-09 16:30:33 +0200

    Add explaination pages (about, links, ...)

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   resources :tracers, only: [:index, :show]
   resources :reports, only: [:index, :show, :create, :new]
 
+  get '/security', to: 'pages#security'
+  get '/about', to: 'pages#about'
+  get '/links', to: 'pages#links'
+  get '/ansel', to: 'pages#ansel'
+  get '/disclaimer', to: 'pages#disclaimer'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
