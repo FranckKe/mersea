3b3e6442fc7445abb9c00ed3d29807fd1c475a89
Pierre Charpentier <charpentier.pierre@outlook.com>
2017-01-12 21:54:52 +0100

    Migrate database to add icon_url to tracers

---
+++ db/migrate/20161214214800_add_icon_url_to_tracers.rb
@@ -0,0 +1,5 @@
+class AddIconUrlToTracers < ActiveRecord::Migration                                                 
+  def change                                                                                        
+    add_attachment :tracers, :icon_url                                                              
+  end                                                                                        
+end
