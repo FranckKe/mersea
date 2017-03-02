e69a5fc3996ad866ea1b06c459c8ad0f3f7de4c3
mdouchement <marc.douchement@predicsis.com>
2017-03-02 20:09:20 +0100

    Add status route for health check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :fetch_pages, only: [:new, :index, :show]
 
+  def status
+    render json: {}, status: :ok
+  end
+
   private
 
   def fetch_pages
