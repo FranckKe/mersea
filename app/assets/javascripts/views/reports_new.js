171bd470d6ff8fb9420a2d1fb62949ca143efd98
Franck Kerbiriou <franck.kerbiriou@gmail.com>
2017-03-08 23:56:51 +0100

    Remove exif and file validation

--- app/assets/javascripts/views/reports_new.js
+++ app/assets/javascripts/views/reports_new.js
@@ -1,12 +1,3 @@
-function validateFileSize() {
-  var file = $('#new_report form input img');
-  return typeof files != 'undefined' && filesize === 1;
-}
-
-$('.report-form form').submit(function () {
-  // validateFileSize();
-});
-
 function populateLatLng() {
   $('#report_latitude').val(localStorage.getItem('clickedLat') || localStorage.getItem('lat'));
   $('#report_longitude').val(localStorage.getItem('clickedLng') || localStorage.getItem('lng'));
