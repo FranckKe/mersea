f1821914b93046c5a7e8052ac704f5843023a100
mdouchement <marc.douchement@predicsis.com>
2017-05-13 20:15:38 +0200

    Add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admin
 
   match '/status', to: 'application#status', via: %i(get head)
@@ -6,6 +7,14 @@ Rails.application.routes.draw do
   resources :tracers, only: [:index, :show]
   resources :reports, only: [:index, :create, :new]
   resources :pages, only: [:show, :index]
+
+  resources :users, only: [:get, :patch] do
+    collection do
+      get 'me'
+      patch 'me'
+      patch 'update_password', defaults: { format: 'json' }
+    end
+  end
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
