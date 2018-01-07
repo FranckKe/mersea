e2e97bf5cce7d6883f8c1d82ff7d99b2560fc492
Franck Kerbiriou <franck.kerbiriou@gmail.com>
2018-01-07 17:02:32 +0100

    QoL changes
    Remove marker when closing report creation popup
    Add pointer cursor to report editing
    Cache leaderboard only if it is not empty

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,6 +12,6 @@ class PagesController < ApplicationController
         reports_count: user.reports.count,
         last_activity: user.reports.last.reported_at }
     end || []
-    Rails.cache.write('leaderboard', @leaderboard, expires_in: 12.hours)
+    Rails.cache.write('leaderboard', @leaderboard, expires_in: 12.hours) unless @leaderboard.empty?
   end
 end
