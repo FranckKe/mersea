07da1ccf7ef12b6adb8b7b7ef12a9ce90c7e9f99
mdouchement <marc.douchement@gmail.com>
2018-03-19 15:41:40 +0100

    Fix migrations

--- db/migrate/20161214214800_add_icon_url_to_tracers.rb
+++ db/migrate/20161214214800_add_icon_url_to_tracers.rb
@@ -1,4 +1,4 @@
-class AddIconUrlToTracers < ActiveRecord::Migration
+class AddIconUrlToTracers < ActiveRecord::Migration[5.0]
   def change
     add_attachment :tracers, :icon_url
   end
--- db/schema.rb
+++ db/schema.rb
@@ -18,87 +18,87 @@ ActiveRecord::Schema.define(version: 20170513194900) do
   enable_extension "citext"
 
   create_table "admins", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.string   "name"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_admins_on_email", using: :btree
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email"
   end
 
   create_table "pages", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.string   "name"
-    t.text     "content"
+    t.string "name"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "reports", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.uuid     "tracer_id"
-    t.string   "name"
-    t.string   "email"
-    t.integer  "quantity"
-    t.string   "address"
-    t.float    "longitude"
-    t.float    "latitude"
-    t.text     "description"
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
+    t.uuid "tracer_id"
+    t.string "name"
+    t.string "email"
+    t.integer "quantity"
+    t.string "address"
+    t.float "longitude"
+    t.float "latitude"
+    t.text "description"
+    t.string "photo_file_name"
+    t.string "photo_content_type"
+    t.integer "photo_file_size"
     t.datetime "photo_updated_at"
-    t.date     "reported_at"
-    t.string   "status"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.uuid     "user_id"
-    t.index ["address"], name: "index_reports_on_address", using: :btree
-    t.index ["latitude"], name: "index_reports_on_latitude", using: :btree
-    t.index ["longitude"], name: "index_reports_on_longitude", using: :btree
-    t.index ["reported_at"], name: "index_reports_on_reported_at", using: :btree
-    t.index ["status"], name: "index_reports_on_status", using: :btree
-    t.index ["tracer_id"], name: "index_reports_on_tracer_id", using: :btree
-    t.index ["user_id"], name: "index_reports_on_user_id", using: :btree
+    t.date "reported_at"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "user_id"
+    t.index ["address"], name: "index_reports_on_address"
+    t.index ["latitude"], name: "index_reports_on_latitude"
+    t.index ["longitude"], name: "index_reports_on_longitude"
+    t.index ["reported_at"], name: "index_reports_on_reported_at"
+    t.index ["status"], name: "index_reports_on_status"
+    t.index ["tracer_id"], name: "index_reports_on_tracer_id"
+    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "tracers", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
+    t.string "name"
+    t.text "description"
+    t.string "photo_file_name"
+    t.string "photo_content_type"
+    t.integer "photo_file_size"
     t.datetime "photo_updated_at"
-    t.string   "origin"
-    t.string   "kind"
-    t.float    "longitude"
-    t.float    "latitude"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "color"
+    t.string "origin"
+    t.string "kind"
+    t.float "longitude"
+    t.float "latitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "color"
   end
 
   create_table "users", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.string   "name"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", using: :btree
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
