9772570bd20c6678224a88c433ec0a2cb5fac2f0
Qayou <charpentier.pierre@outlook.com>
2018-04-21 13:22:29 +0200

    Merge changes with pu-2.0

--- app/mersea-front/src/App.vue
+++ app/mersea-front/src/App.vue
@@ -1,20 +1,107 @@
 <template>
-  <div class="app">
-    <header>
-      <h1>{{ appName }}</h1>
-    </header>
-    <main>
-      <router-view></router-view>
-    </main>
+  <div id="app">
+    <Slideout menu="#menu" panel="#panel" :toggleSelectors="['.toggle-button']" @on-open="open">
+      <nav id="menu">
+        <div>Menu</div>
+        <aside class="menu">
+          <p class="menu-label">
+            General
+          </p>
+          <ul class="menu-list">
+            <li>
+              <a>Dashboard</a>
+            </li>
+            <li>
+              <a>Customers</a>
+            </li>
+          </ul>
+          <p class="menu-label">
+            Administration
+          </p>
+          <ul class="menu-list">
+            <li>
+              <a>Team Settings</a>
+            </li>
+            <li>
+              <a class="is-active">Manage Your Team</a>
+              <ul>
+                <li>
+                  <a>Members</a>
+                </li>
+                <li>
+                  <a>Plugins</a>
+                </li>
+                <li>
+                  <a>Add a member</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Invitations</a>
+            </li>
+            <li>
+              <a>Cloud Storage Environment Settings</a>
+            </li>
+            <li>
+              <a>Authentication</a>
+            </li>
+          </ul>
+          <p class="menu-label">
+            Transactions
+          </p>
+          <ul class="menu-list">
+            <li>
+              <a>Payments</a>
+            </li>
+            <li>
+              <a>Transfers</a>
+            </li>
+            <li>
+              <a>Balance</a>
+            </li>
+          </ul>
+        </aside>
+      </nav>
+      <header>
+        <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+          <div class="navbar-start">
+            <div class="navbar-item">
+              <button class="button toggle-button">☰</button>
+            </div>
+            <a class="navbar-item" href="#">
+              Ocean Plastic Tracker
+            </a>
+          </div>
+        </nav>
+        <!--<div>
+          <button class="navbar-burger toggle-button">☰</button>
+        </div>-->
+      </header>
+
+      <main id="panel">
+        <img src="./assets/logo.png" alt="Vue.js PWA">
+        <router-view></router-view>
+      </main>
+    </Slideout>
   </div>
 </template>
 
 <script>
+import Slideout from 'vue-slideout'
+
 export default {
   name: 'app',
   data() {
     return {
       appName: this.$appName
+    }
+  },
+  components: {
+    Slideout
+  },
+  methods: {
+    open: function() {
+      // console.log('slideoutOpen')
     }
   }
 }
@@ -55,14 +142,48 @@ header {
   align-items: center;
 }
 
-header span {
-  display: block;
+#panel {
+  margin-top: 40px;
+}
+
+.slideout-menu {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+  z-index: 0;
+  display: none;
+  background-color: #1d1f20;
+  color: white;
+}
+
+.slideout-menu-left {
+  left: 0;
+}
+
+.slideout-menu-right {
+  right: 0;
+}
+
+.slideout-panel {
+  background-color: #4b5;
+  color: white;
   position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: 0.02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
+  z-index: 1;
+  will-change: transform;
+  min-height: 100vh;
+}
+
+.slideout-open,
+.slideout-open body,
+.slideout-open .slideout-panel {
+  overflow: hidden;
+}
+
+.slideout-open .slideout-menu {
+  display: block;
 }
 </style>
