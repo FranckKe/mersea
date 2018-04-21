56f302063313b2225f2e1ffd3eb4bec2f5678754
Qayou <charpentier.pierre@outlook.com>
2018-04-21 12:10:31 +0200

    Prioritizes standard over prettier

--- app/mersea-front/.eslintrc.js
+++ app/mersea-front/.eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     browser: true
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: ['prettier', 'standard'],
+  extends: ['standard', 'prettier'],
   // required to lint *.vue files
   plugins: ['html', 'prettier'],
   // add your custom rules here
