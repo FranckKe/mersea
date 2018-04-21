e6900a036ad7196eac7ee0fe15808ed3a7e8a37a
Franck Kerbiriou <franck.kerbiriou@gmail.com>
2018-04-21 16:58:35 +0200

    Add Mapbox
    Add map search bar
    Add map geolocalization
    Add map zoom
    Add map scale
    Whitelist mapbox-gl from webpack
    Add topbar
    Add colors

--- app/mersea-front/src/components/Home.vue
+++ app/mersea-front/src/components/Home.vue
@@ -1,23 +1,73 @@
 <template>
   <div class="">
+  <div id="map"></div>
   </div>
 </template>
 
 <script>
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
+
 export default {
-  name: 'home',
   data() {
-    return {
-      msg: 'Welcome'
+    return {}
+  },
+  mounted() {
+    this.createMap()
+    console.dir(mapboxgl)
+  },
+  methods: {
+    createMap: function() {
+      mapboxgl.accessToken =
+        'pk.eyJ1IjoiZnJhbmNrayIsImEiOiJjamc5ODhrazUzaXlvMndvaDBzMnZoZXF6In0.ThvS99eoVrbmTC_KAmv_6w'
+      this.map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/satellite-streets-v9',
+        minzoom: 2,
+        zoom: 5,
+        center: [0, 46.2276]
+      })
+
+      this.map.addControl(new mapboxgl.ScaleControl())
+      this.map.addControl(
+        new MapboxGeocoder(
+          {
+            accessToken: mapboxgl.accessToken
+          },
+          'top'
+        ),
+        'top-left'
+      )
+      this.map.addControl(
+        new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true
+          },
+          trackUserLocation: true
+        })
+      )
+      this.map.addControl(new mapboxgl.NavigationControl(), 'bottom-right')
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-mapbox {
+#map {
   width: 100%;
-  height: 100vh;
+  height: calc(100vh - var(--header-height));
+}
+
+.mapboxgl-ctrl-group > button {
+  width: 40px;
+  height: 40px;
+}
+
+.mapboxgl-ctrl-top-left,
+.mapboxgl-ctrl-geocoder {
+  width: calc(100% - 35px);
+  max-width: calc(100% - 35px);
 }
 </style>
