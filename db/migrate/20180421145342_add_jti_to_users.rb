f4b763415f6bfc04f6f56eaa1c1a0ca1e8156725
mdouchement <marc.douchement@gmail.com>
2018-04-25 16:21:44 +0200

    Fix migration

--- db/migrate/20180421145342_add_jti_to_users.rb
+++ db/migrate/20180421145342_add_jti_to_users.rb
@@ -1,7 +1,7 @@
 class AddJtiToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :jti, :string
-    User.all.each { |user| user.update_column(:jti, Warden::JWTAuth::UserEncoder.new.call(User.first, :user, nil).first) }
+    User.all.each { |user| user.update_column(:jti, Warden::JWTAuth::UserEncoder.new.call(user, :user, nil).first) }
     change_column_null :users, :jti, false
     add_index :users, :jti, unique: true
   end
