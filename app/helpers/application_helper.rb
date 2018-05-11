62e6d42e34955dd5d15267eff660a4bc73809728
mdouchement <marc.douchement@gmail.com>
2018-05-11 16:46:55 +0200

    Restore Flash message for Rails Admin by disabling rails-api mode

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def bulma_flash_message(status)
+    case status
+    when 'alert'
+      'is-danger'
+    when 'notice'
+      'is-info'
+    else
+      ''
+    end
+  end
 end
