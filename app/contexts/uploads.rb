31f9f5701ba0186a55ae393e5c357732c81549ff
mdouchement <marc.douchement@gmail.com>
2018-06-04 15:56:06 +0200

    Rails Admin now displays the photos

--- app/contexts/uploads.rb
+++ app/contexts/uploads.rb
@@ -1,5 +1,6 @@
 class Uploads
   REGEXP = /\Adata:(?<type>[-\w]+\/[-\w\+\.]+)?;base64,(?<content>.*)/m
+  EXT = /image\/(?<ext>\w+).*/
 
   attr_reader :data_uri
 
@@ -13,10 +14,11 @@ class Uploads
 
   def call
     data_uri_parts = data_uri.match(REGEXP) || {}
+    ext = ((data_uri_parts[:type] || '').match(EXT) || {})[:ext] || '.png'
     {
       io: StringIO.new(Base64.decode64(data_uri_parts[:content] || '')),
       content_type: data_uri_parts[:type],
-      filename: SecureRandom.uuid
+      filename: "#{SecureRandom.uuid}.#{ext}"
     }
   end
 end
