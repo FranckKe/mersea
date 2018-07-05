761ce06d4861c77b06023fe4197ef5f30a48a60f
Pierre Charpentier <charpentier.pierre@outlook.com>
2018-07-05 20:43:01 +0200

    Conflict between photo and photo_url in tracer scheme

--- app/serializers/tracer_serializer.rb
+++ app/serializers/tracer_serializer.rb
@@ -1,14 +1,10 @@
 class TracerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :color, :photo, :photo_url
+  attributes :id, :name, :color, :photo
 
   def photo
     variant = object.photo.variant(resize: '256x256>').processed
     Rails.application.routes.url_helpers.rails_blob_representation_path(
       variant.blob.signed_id, variant.variation.key, variant.blob.filename, only_path: true
     )
-  end
-
-  def photo_url
-    object.photo.url
   end
 end
