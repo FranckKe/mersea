63363ec84795e5d8fb7f065eca8ade992225ad84
mdouchement <marc.douchement@gmail.com>
2018-07-05 22:58:27 +0200

    Update spec

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -5,10 +5,12 @@ describe Page do
   it { expect(subject).to have_db_column(:name) }
   it { expect(subject).to have_db_column(:content) }
   it { expect(subject).to have_db_column(:language) }
+  it { expect(subject).to have_db_column(:category) }
   it { expect(subject).to have_db_column(:created_at) }
   it { expect(subject).to have_db_column(:updated_at) }
 
   it { expect(subject).to validate_presence_of(:name) }
   it { expect(subject).to validate_presence_of(:language) }
+  it { expect(subject).to validate_presence_of(:category) }
   it { expect(subject).to validate_presence_of(:content) }
 end
