5651b56abeee5eb3271687c1795bce91732835fd
Franck Kerbiriou <franck.kerbiriou@gmail.com>
2018-07-11 23:21:23 +0200

    Vue cli 3

---
+++ frontend/src/router.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home";
+import Leaderboard from "./views/Leaderboard";
+import Tracers from "./views/Tracers";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/leaderboard",
+      name: "Leaderboard",
+      component: Leaderboard
+    },
+    {
+      path: "/tracers",
+      name: "Tracers",
+      component: Tracers
+    }
+  ]
+});
