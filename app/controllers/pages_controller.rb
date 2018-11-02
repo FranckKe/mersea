be3a42cc62d8471ced48442891917e53511cf6b0
mdouchement <marc.douchement@gmail.com>
2018-11-02 21:10:26 +0100

    Cache /reports & /leaderboard with nginx

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,20 +10,12 @@ class PagesController < ApiController
   end
 
   def leaderboard
-    @leaderboard = Rails.cache.fetch('leaderboard')
-    fetch_leaderboard unless @leaderboard
-    render json: @leaderboard
-  end
-
-  private
-
-  def fetch_leaderboard
     users = User.joins(:reports).group('users.id').having('count(users.id) > 0')
     @leaderboard = users.map do |user|
       { name: user.name,
         reports_count: user.reports.count,
         last_activity: user.reports.last.reported_at }
-    end || []
-    Rails.cache.write('leaderboard', @leaderboard, expires_in: 12.hours) unless @leaderboard.empty?
+      end || []
+    render json: @leaderboard
   end
 end
