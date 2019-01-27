f51e4c522fd2faf0275b096d013e9473b1ca7916
mdouchement <marc.douchement@gmail.com>
2019-01-27 11:50:12 +0100

    Fix disabled recaptcha in production

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,6 @@
 Recaptcha.configure do |config|
   config.site_key = Rails.application.secrets.recaptcha[:site_key]
   config.secret_key = Rails.application.secrets.recaptcha[:secret_key]
-  config.skip_verify_env << ['development', 'test', 'production']
+  config.skip_verify_env << 'development'
+  config.skip_verify_env << 'production'
 end
