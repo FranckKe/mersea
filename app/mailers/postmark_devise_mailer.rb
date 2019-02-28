dc36a07f91bc41806d16061a900268153bb34761
Franck Kerbiriou <franck.kerbiriou@gmail.com>
2019-02-28 23:06:33 +0100

    Build uri

--- app/mailers/postmark_devise_mailer.rb
+++ app/mailers/postmark_devise_mailer.rb
@@ -2,16 +2,16 @@ class PostmarkDeviseMailer < Devise::Mailer
   FROM_EMAIL = Rails.application.secrets.postmark[:from]
   TEMPLATE_IDS = Rails.application.secrets.postmark[:template_ids]
   CLIENT = Postmark::ApiClient.new(Rails.application.secrets.postmark[:api_key])
+  HOST = URI(Rails.application.secrets.host)
 
   def reset_password_instructions(record, token, _)
     case record
     when User
-      params = {
-        reset_password_token: token
-      }
-      url = "#{Rails.application.secrets.host}/#/users/reset_password#{params.to_query}"
+      uri = URI.join(HOST, '#', 'users', 'reset_password')
+      uri.query = {reset_password_token: token}.to_query
+      uri.to_s
     when Admin
-      url = edit_admin_password_url(record, reset_password_token: token)
+      uri = edit_admin_password_url(record, reset_password_token: token)
     end
 
     CLIENT.deliver_with_template(
@@ -21,7 +21,7 @@ class PostmarkDeviseMailer < Devise::Mailer
       template_model: {
         subject: I18n.t(:subject, scope: 'devise.mailer.reset_password_instructions'),
         name: record.name,
-        reset_password_url: url
+        reset_password_url: uri
       }
     )
   end
