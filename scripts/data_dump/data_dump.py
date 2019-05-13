e0be7f321fe6272c52ab91aa63cf2c13a6ca6e23
Franck Kerbiriou <franck.kerbiriou@gmail.com>
2019-05-13 18:21:30 +0200

    Refactor

--- scripts/data_dump/data_dump.py
+++ scripts/data_dump/data_dump.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python
+ #!/usr/bin/env python
 # coding: utf-8
-
-# In[5]:
-
 
 import requests
 import pandas as pd
@@ -17,133 +14,120 @@ from os.path import join, dirname, realpath
 from dotenv import load_dotenv
 import os
 from json import JSONDecodeError
-from requests.adapters import HTTPAdapter 
+from requests.adapters import HTTPAdapter
 
-
-# In[6]:
-
-
-#lecture du fichier .env pour récupérer les paramètres de config
-dotenv_path = join(dirname(realpath('__file__')),'.env.path')
+dotenv_path = join(dirname(realpath('__file__')), '.env')
 load_dotenv(dotenv_path)
 
-urlAPI = os.getenv('URL_API')
-csvPath = os.getenv('CSV_PATH')
-jsonPath = os.getenv('JSON_PATH')
-geojsonPath = os.getenv('GEOJSON_PATH')
+URL_API = os.getenv('URL_API')
+DATA_DIR = path("./data")
+MAX_RETRY = 5
+TIMEOUT = 5
 
-
-# In[7]:
-
+# TODO
+# - Use path concatenation and/or os.path.join (ctrl+f path)
+# - Add base url to photos url in tracers data
+# - Create function for each calls (reports(json), reports(geojson), tracers, upload_file)
 
 logging.basicConfig(format='%(process)d-%(levelname)s-%(message)s')
 
 logger = logging.getLogger()
-#les headers du json et geojson
-headers_json = {"Content-Type" : "application/json"}
-headers_geojson = {"Content-Type" : "application/json",
-           "Accept" : "application/geo+json",
-           "Cache-Control" : "no-cache"}
+headers_json = {"Content-Type": "application/json"}
+headers_geojson = {"Content-Type": "application/json",
+                   "Accept": "application/geo+json",
+                   "Cache-Control": "no-cache"}
 
 session = requests.Session()
-#retry une seule fois si le status est 500, 503, 504 (est-ce que tu veux d'autres code status pour le force du retry ?)
-session.mount('https://', HTTPAdapter(
-    max_retries=Retry(total=1, status_forcelist=[500, 503, 504, 404])
-    )
-)
-try:
-    response_json = session.get(urlAPI,headers=headers_json, timeout=6)
-    if response_json.status_code not in [200, 201, 202, 203, 204, 205, 206]:
-        logger.error("le status du get est : " + str(response_json.status_code))
-        sys.exit
-    try:
-        data_json = response_json.json()
+session.mount(HTTPAdapter(max_retries=MAX_RETRY))
 
-        #suppression des propreties status, name et address du json
-        for proprety in data_json:
+try:
+    response_json = session.get(
+        URL_API + "/reports", headers=headers_json, timeout=TIMEOUT).raise_for_status()
+    try:
+        json_reports = response_json.json()
+
+        logger.info("Json reports downloaded")
+
+        # Anonymize data
+        for proprety in json_reports:
             del proprety['name']
             del proprety['status']
             del proprety['address']
 
-        json_to_df = pd.read_json(json.dumps(data_json))
+        json_to_df = pd.read_json(json.dumps(json_reports))
 
-        #écrire le résultat du json dans un csv
-        json_to_df.to_csv(csvPath, encoding='utf-8', index=False)
-        config = Path(csvPath) 
-        if config.is_file():
-            print('charger le fichier dans data gouv' + csvPath)
-        else:
-            #raise FileNotFoundError("fichier n'exsite pas ou n'a pas été créé")
-            logger.error("fichier suivant : " + csvPath +" n'exsite pas ou n'a pas été créé")
-            sys.exit
+        json_to_df.to_csv(path("DATA_DIR", "/reports.csv"),
+                          encoding='utf-8', index=False)
 
-        #écrire le resultats de la requête http dans un fichier json
-        with open(jsonPath, 'w') as f:
-            #indent permet d'ajouter une nouvelle ligne et une tabulation
-            json.dump(data_json, f, indent=3)
-        #vérifier si le fichier a bien été crée si non on lance une exception
-        config = Path(jsonPath) 
-        if config.is_file():
-            print('charger le fichier dans data gouv' + jsonPath)
-        else:
-            #raise FileNotFoundError("fichier n'exsite pas ou n'a pas été créé")
-            logger.error("fichier suivant : " + jsonPath +" n'exsite pas ou n'a pas été créé")
-            sys.exit
+        with open(path("DATA_DIR", "reports.json"), 'w') as f:
+            json.dump(json_reports, f)
+
+        logger.info("Json reports written to disk")
+
     except JSONDecodeError as e:
-        logger.error("no data returned")
+        logger.error("Empty response during reports retrieval")
         logger.error(e)
         sys.exit
-        
-except Timeout as e:
-    logger.error('le time out es dépassé')
-    logger.error(e)
-    sys.exit
-    
-except RetryError as e:
-    logger.error('le serveur ne répond pas')
-    logger.error(e)
-    sys.exit
-      
-#est-ce que tu veux deux scripts différents pour le geojson et json (csv) ou je mets tous dans le même script ?
-try:     
-    response_geojson = session.get(urlAPI,headers=headers_geojson, timeout=6)
-    if response_geojson.status_code not in [200, 201, 202, 203, 204, 205, 206]:
-        logger.error("le status du get est : " + str(response_geojson.status_code))
-        sys.exit
-    try:
-        data_geo_json = response_geojson.json()
 
-        #suppression de user dans les résulats
-        for prop in data_geo_json['features']:
+except:
+    logger.error('Error during json reports retrieval')
+    logger.error(e)
+    sys.exit
+
+
+try:
+    response_geojson = session.get(
+        URL_API + "/reports", headers=headers_geojson, timeout=TIMEOUT).raise_for_status
+    try:
+        geojson_reports = response_geojson.json()
+
+        logger.info("Geojson reports downloaded")
+
+        # Anonymize data
+        for prop in geojson_reports['features']:
             del prop['properties']['user']
 
-        #écrire le resultats de la requête http dans un fichier json
-        with open(geojsonPath, 'w') as f:
-            #indent permet d'ajouter une nouvelle ligne et une tabulation
-            json.dump(data_geo_json, f, indent=3)
-        #vérifier si le fichier a bien été crée si non on lance une exception
-        config = Path(geojsonPath) 
-        if config.is_file():
-            print('charger le fichier dans data gouv' + geojsonPath)
-        else:
-            logger.error("Le fichier suivant : "+ geojsonPath +" n'exsite pas ou n'a pas été créé")
+        with open(path("DATA_DIR", "reports.geojson"), 'w') as file:
+            json.dump(geojson_reports, file)
+
+        logger.info("Geojson reports written to disk")
+
     except JSONDecodeError as e:
-        logger.error("no data returned")
+        logger.error("Empty response during geosjon reports retrieval")
         logger.error(e)
         sys.exit
-except Timeout as e:
-    logger.error('le time out es dépassé')
-    logger.error(e)
-    sys.exit
-    
-except RetryError as e:
-    logger.error('le serveur ne répond pas')
+except:
+    logger.error('Error during geojson reports retrieval')
     logger.error(e)
     sys.exit
 
+try:
+    tracers_response = session.get(
+        URL_API + "/tracers", headers=json_reports, timeout=TIMEOUT).raise_for_status
+    try:
+        json_tracers = tracers_response.json()
 
-# In[ ]:
+        logger.info("Json tracers downloaded")
 
+        # Remove useless data
+        for proprety in json_reports:
+            del proprety['color']
 
+        # TODO Add url to each photo
 
+        with open(path("DATA_DIR", "reports.geojson"), 'w') as file:
+            json.dump(json_tracers, file)
 
+        logger.info("Tracers reports written to disk")
+
+    except JSONDecodeError as e:
+        logger.error("Empty response during json tracers retrieval")
+        logger.error(e)
+        sys.exit
+except:
+    logger.error('Error during json tracers retrieval')
+    logger.error(e)
+    sys.exit
+
+# For json and csv in path("DATA_DIR")
+    # Upload file on data gouv
