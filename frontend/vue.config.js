74fb77c25d6af4d5031dd285c5e5a35edeadafc3
Franck Kerbiriou <franck.kerbiriou@gmail.com>
2019-05-25 14:21:51 +0200

    Remove purgecss as it breaks mapbox plugin
    Add nginx gunzip

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,23 +1,9 @@
-const PurgecssPlugin = require('purgecss-webpack-plugin')
-const glob = require('glob-all')
-const path = require('path')
-
 module.exports = {
   productionSourceMap: false,
   configureWebpack: {
-    plugins: [
-      new PurgecssPlugin({
-        paths: glob.sync([
-          path.join(__dirname, './src/index.html'),
-          path.join(__dirname, './**/*.vue'),
-          path.join(__dirname, './src/**/*.js')
-        ])
-      })
-    ],
     optimization: {
       splitChunks: {
-        minSize: 10000,
-        maxSize: 1250000
+        maxSize: 100000
       }
     }
   },
