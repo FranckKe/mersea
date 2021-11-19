f57cf760df22d2198bb4aef3792d38499a41bb3f
Franck Kerbiriou <contact@franckkerbiriou.com>
2021-11-19 20:35:40 +0100

    Add Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_202836) do
+ActiveRecord::Schema.define(version: 2021_11_19_200736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
