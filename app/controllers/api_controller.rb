823fee3478c31f6dd5a958f1ba28afbdbe35d718
mdouchement <marc.douchement@gmail.com>
2022-03-11 11:15:54 +0100

    Revert "Workaround HEAD /status (#371)"
    This reverts commit 3571e48fe2882491afb80a0f42b56cb35a7fd7d4.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,10 +7,6 @@ class ApiController < ActionController::API
 
   def status
     render json: {}, status: :ok
-  rescue ActionDispatch::Http::MimeNegotiation::InvalidType
-    # FIXME: find a better way to handle this case.
-    # `set_default_format' should avoid this case.
-    render json: {}, status: :ok
   end
 
   rescue_from StandardError do |e|
